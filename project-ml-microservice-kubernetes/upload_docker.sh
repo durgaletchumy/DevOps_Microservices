c8ed9722ea4847a545cc95171c5147735359118e
EC2 Default User <ec2-user@ip-172-31-81-202.ec2.internal>
2020-04-15 17:11:30 +0000

    Task 3 and 4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="durgaletchumy/prediction"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag 3283d180e16b durgaletchumy/prediction
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push durgaletchumy/prediction:latest
